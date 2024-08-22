80d5e203f9ce3d6e07e49c85a2a901baffc72308
Fabiano De Paula <fabiano.rpaula@gmail.com>
2024-08-22 17:41:41 -0300

    Ia Fps Atualizado

--- Assets/Scripts/IaFps.cs
+++ Assets/Scripts/IaFps.cs
@@ -7,30 +7,67 @@ public class IaFps : MonoBehaviour
 {
 
     private NavMeshAgent agente;
+    private Animator animacao;
     public List<GameObject> Destinos;
     public int objeto = 0;
+    public float tempo = 0;
+
+    //Maquina de Estados
+    public enum Estados { Parado, Ronda};
+    public Estados MeuEstado;
 
     void Start()
     {
         agente = GetComponent<NavMeshAgent>();
+        animacao = GetComponent<Animator>();
+        MeuEstado = Estados.Parado;
     }
 
     void Update()
     {
-        
+        //Controlador de Estados
+        if(MeuEstado == Estados.Parado)
+        {
+            Parado();
+        }
+        if (MeuEstado == Estados.Ronda)
+        {
+            Ronda();
+        }
+      
+      
+    }
+
+    void Parado()
+    {
+        animacao.SetBool("Ronda", false);
+        agente.speed = 0;
+        tempo += Time.deltaTime;
+        if (tempo > 0.3) {
+            tempo = 0;
+            //Altero Estado
+            MeuEstado = Estados.Ronda;
+        }
     }
 
     void Ronda()
     {
+        //Coloca a animação de Ronda
+        animacao.SetBool("Ronda", true);
+        agente.speed = 30;
+        //Move o Personagem até o destino
         agente.SetDestination(Destinos[objeto].
             transform.position);
-
+        //Calcula Distancia até o destino
         float DistanciaPonto = Vector3.Distance(
             transform.position,
             Destinos[objeto].transform.position);
-        if(DistanciaPonto < 10)
+        //Se a distancia menor que 10
+        if(DistanciaPonto < 8)
         {
+            //Sortei um novo destino
             objeto = Random.Range(0, Destinos.Count);
+            MeuEstado = Estados.Parado;
         }
     }
 }
